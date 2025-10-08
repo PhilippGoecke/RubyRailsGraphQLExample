55c649fcc447148bf9abe9d149fd0adc8726bd2c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-10-08 23:08:33 +0200

    Update podman-run-backend.bash

--- BackEnd/podman-run-backend.bash
+++ BackEnd/podman-run-backend.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm --file Containerfile --tag graphql:backend .
-podman run --interactive --tty --publish 3000:3000 graphql:backend
-echo "browse https://localhost:3000/graphql"
+podman run --interactive --tty --publish 3333:3000 graphql:backend
+echo "browse https://localhost:3333/graphiql"
