4590646d4293fee4e1a7b8be3b1f7050e4f5228e
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-10-11 23:03:18 +0200

    Update sed command to modify nginx.conf instead of Containerfile

--- podman-compose.bash
+++ podman-compose.bash
@@ -1,4 +1,4 @@
 # podman system prune -a -f
-sed -i 's/host.containers.internal:3333/backend:3000/g' FrontEnd/Containerfile
+sed -i 's/host.containers.internal:3333/backend:3000/g' FrontEnd/nginx.conf
 podman-compose --file podman-compose.yml up -d
 podman-compose --file podman-compose.yml logs --follow
