db5b750b88361c65c55741dd96381e871ea4a8ec
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-10-11 23:29:37 +0200

    Comment out frontend and backend log commands
    Comment out log commands for frontend and backend.

--- podman-compose.bash
+++ podman-compose.bash
@@ -2,3 +2,5 @@
 sed -i 's/host.containers.internal:3333/backend:3000/g' FrontEnd/nginx.conf
 podman-compose --file podman-compose.yml up -d
 podman-compose --file podman-compose.yml logs --follow
+#podman-compose --file podman-compose.yml logs frontend --follow
+#podman-compose --file podman-compose.yml logs backend --follow
